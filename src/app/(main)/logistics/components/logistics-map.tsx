3c92caba2da80f128bcd8929a74654789ce91d2a
Daksh Ranjan Srivastava (BTech CSE 2025-29) <2025.dakshr@isu.ac.in>
2025-11-22 16:52:04 +0000

    ok i would like to add satellite view in this particular map

--- src/app/(main)/logistics/components/logistics-map.tsx
+++ src/app/(main)/logistics/components/logistics-map.tsx
@@ -92,9 +92,24 @@ export function LogisticsMap() {
         scrollWheelZoom: true,
       });
 
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      const standardLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(mapRef.current);
+
+      const satelliteLayer = L.tileLayer(
+        'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+        {
+          attribution:
+            'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+        }
+      );
+
+      const baseMaps = {
+        'Standard View': standardLayer,
+        'Satellite View': satelliteLayer,
+      };
+
+      L.control.layers(baseMaps).addTo(mapRef.current);
 
       hospitals.forEach((hospital: Hospital) => {
         L.marker([hospital.lat, hospital.lng])
